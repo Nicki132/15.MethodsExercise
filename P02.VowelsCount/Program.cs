c2f8686e2c14b7567afac61a07a186dd8e5d5486
Nikolay Stoyanov <nikitig1987@abv.bg>
2022-11-18 15:55:00 +0100

    Add project files.

---
+++ P02.VowelsCount/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read single string from Console
+            string word = Console.ReadLine();
+
+            //TODO: Creates method which return vowels count
+            int vowelsCount = GetVowelsCount(word);
+
+            //Print vowels count
+            Console.WriteLine(vowelsCount);
+        }
+
+        static int GetVowelsCount(string word)
+        {
+            //Creates array with all vowels characters
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+            int vowelsCount = 0;
+            foreach (char ch in word.ToLower())
+            {
+                if (vowels.Contains(ch))
+                {
+                    //Current character is a vowel
+                    vowelsCount++;
+                }
+            }
+
+            return vowelsCount;
+        }
+    }
+}
---
+++ P20Exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P20Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string values = Console.ReadLine();
+            string[] items = values.Split();
+            int[] arr = new int[items.Length];
+            for (int i = 0; i < items.Length; i++)
+            {
+                arr[i] = int.Parse(items[i]);
+            }
+        }
+    }
+}
